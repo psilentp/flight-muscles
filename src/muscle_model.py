363bc8080d4c1113df708c97cc213ab300a6deca
Flyranch <flyranch@flyranch-OptiPlex-990.(none)>
2015-02-04 17:31:41 -0800

    cleaned up the transformation code now I can implement a generalized warp function

--- src/muscle_model.py
+++ src/muscle_model.py
@@ -14,9 +14,31 @@ class Frame(dict):
                 dict.__setitem__(self,key,item)
         except KeyError:
             dict.__setitem__(self,key,item)
-                        
+
+    def load(self,filepath):
+        """load from a pickled dictionary"""
+        import cPickle
+        pkname = filepath
+        f = open(pkname);data = cPickle.load(f);f.close()
+        for key in data.keys():
+            self[key] = data[key]
+
+    def get_transform(self,other):
+        """get transform into self from other frame"""
+        A1 = np.hstack((self['A'],self['p'][:,np.newaxis]))
+        A2 = np.hstack((other['A'],other['p'][:,np.newaxis]))
+        A1 = np.vstack((A1,[0,0,1]))
+        A2 = np.vstack((A2,[0,0,1]))
+        return(np.dot(A1,np.linalg.inv(A2)))
+
 class GeometricModel(object):   
-    def __init__(self,lines,frame):
+    def __init__(self,lines = None,frame = None,filepath = None):
+        if not(filepath is None):
+            self.load(filepath)
+        else:
+            self.construct(lines,frame)
+
+    def construct(self,lines,frame):
         self.lines = lines
         self.frame = frame
         ## put lines in barycentric coords
@@ -25,6 +47,28 @@ class GeometricModel(object):
             coords = np.dot(self.frame['A_inv'],(self.lines[key]-self.frame['p'][:,np.newaxis])) 
             self.barycentric[key] = coords.T
             
+    def load(self,filepath):
+        """load from a pickled dictionary"""
+        import cPickle as cpkl
+        fi = open(filepath)
+        model_data = cpkl.load(fi)
+        fi.close()
+        model_data['e1'] = np.array([[ 170.02788104,  326.71685254],
+                             [ 380.98203222,  919.92627014]])
+        model_data['e2'] = np.array([[ 172.83333333,  332.83333333],
+                          [ 551.5       ,  164.83333333]])
+        e1 = model_data['e1']
+        e2 = model_data['e2']
+        muscles = dict()
+        for key in model_data.keys():
+            if not(key in ['e1','e2']):
+                muscles[key] = model_data[key]
+        confocal_frame = Frame()
+        confocal_frame['a1'] = e2[1]-e2[0]
+        confocal_frame['a2'] = e1[1]-e2[0]
+        confocal_frame['p'] = e2[0]
+        self.construct(muscles,confocal_frame)
+
     def coords_from_frame(self,other_frame):
         ret = dict()
         for key in self.barycentric.keys():
@@ -41,7 +85,7 @@ class ModelView(object):
         #self.plot_basis['p'] = default_rframe_data['p']
         #self.plot_basis['a1'] = default_rframe_data['a1']
         #self.plot_basis['a2'] = default_rframe_data['a2']
-        
+
     def plot(self,plot_frame,plotobject):
         lines = self.model.coords_from_frame(plot_frame)
         self.curves = list()
