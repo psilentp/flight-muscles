acc7491ff82537bc4e06c673a8ca00baae3f8193
psilentp <psilentp@fly-cluster-master.c.flight-muscles.internal>
2015-05-04 23:25:55 +0000

    added preliminary figure for ptch roll experiment

--- src/group_data.py
+++ src/group_data.py
@@ -3,7 +3,7 @@ import db_access as dba
 fly_db = dba.get_db()
 import numpy as np
 
-GMR22H05_list = [308,309,310,311,312,313,314,315,316,317,327,328,453,455,456,461,462,463,466,467,468,469,470] #R range(317,326) #S
+GMR22H05_list = [308,309,310,311,312,314,315,316,317,327,328,453,455,456,461,462,463,466,467,468,469,470] #R range(317,326) 
 GMR22H05_swarm = flylib.NetSquadron(GMR22H05_list)
 GMR39E01_list = [318,319,320,321,322,323,324,325,329,330,331,332,333,334,335,336] #S
 GMR39E01_swarm = flylib.NetSquadron(GMR39E01_list)
@@ -21,6 +21,8 @@ GMR75B06_swarm = flylib.NetSquadron(GMR75B06_list)
 GMR74F03_list = [396,397,398,411,412,413,414,415,416] #S
 GMR74F03_swarm = flylib.NetSquadron(GMR74F03_list)
 
+GMR22H05_pr_list = [471,472,473,474,475,476,477,478,479,480,481,483,484,485,486]
+GMR22H05_pr_swarm = flylib.NetSquadron(GMR22H05_pr_list)
 ###################
 ###################
 
@@ -58,7 +60,8 @@ swarms = {'GMR22H05':GMR22H05_swarm,
           'GMR29E05_GFP':GMR29E05_GFP_swarm,
           'GMR74F03_GFP':GMR74F03_GFP_swarm,
           'GMR10A12_GFP':GMR10A12_GFP_swarm,
-          'GMR75B06_GFP':GMR75B06_GFP_swarm
+          'GMR75B06_GFP':GMR75B06_GFP_swarm,
+          'GMR22H05_pr':GMR22H05_pr_swarm
          }
 
 exp_swarms = {'GMR22H05':GMR22H05_swarm,
@@ -80,12 +83,18 @@ ctrl_swarms = {
           'GMR75B06_GFP':GMR75B06_GFP_swarm
          }
 
+ptch_roll_swarms = {
+    'GMR22H05_pr':GMR22H05_pr_swarm
+}
+
 encode = {'regressive':1,'descending':2,'progressive':3,'ascending':4,'flow_right':6,'flow_left':5}
 decode = dict()
 
 for key,value in zip(encode.keys(),encode.values()):
     decode[value] = key
-    
+
+
+
 def get_cond(fly_path,trial):
     """extact the experimental condition from a trial"""
     #print 'here'
@@ -98,3 +107,24 @@ def get_cond(fly_path,trial):
     if np.isnan(val):
         raise ValueError
     return val
+
+
+decode_pitch_roll = dict()
+[decode_pitch_roll.update({i:'pth_roll_%s'%p}) for i,p in enumerate(range(0,360,30))]
+
+def get_cond_pitch_roll(fly_path,trial):
+    """extact the experimental condition from a """
+    #print 'here'
+    import h5py
+    fly_record = h5py.File(fly_path + 'fly_record.hdf5')
+    exp_record = fly_record['experiments'].values()[0]
+    sigs = exp_record['tiff_data']['axon_framebase']
+    cond_data = np.array(sigs['StimCond'])[trial] #-1)*360/(30*9)
+    val = np.around(np.mean((cond_data[cond_data>0.5]-1)*360/(30*9)))
+    if np.isnan(val):
+        raise ValueError
+    return val
+
+
+GMR22H05_pr_swarm.get_cond = get_cond_pitch_roll
+GMR22H05_pr_swarm.decode = decode_pitch_roll
